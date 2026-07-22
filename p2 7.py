32abbf9701b1f0b910a3aa5a333918f8587292c2
garimar223-web <garimar223@gmail.com>
2026-07-22 11:55:03 +0530

    Add files via upload

---
+++ p2 7.py
@@ -0,0 +1,13 @@
+#pracs2-Q7
+
+km=float(input("Enter kilometer"))
+celcius=float(input("Enter temp in celcius :"))
+rupees= float(input("Enter rupees"))
+meters=km*1000
+fahrenheit=(celcius*9/5)+32
+dollars= rupees/05
+#Example conversion
+print("Meters =",meters)
+print("Meters =",meters)
+print("Fahrenheit =",fahrenheit)
+print("US dollars =",round (dollars,2))
